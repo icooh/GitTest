2a3aeadbbc79411a0b817692de7904445d379ea7
icooh <icooh@naver.com>
2019-01-07 22:32:01 +0900

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by icooh on 07/01/2019.
+//  Copyright © 2019 myname. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
